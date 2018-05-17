d9d5c0a96ee80e547a0813b6ff61166fd20c30a9
xaxwtf <xaxwtf@gmail.com>
2018-05-17 10:27:34 -0300

    tercer Commit

--- Nomeclatura/main.c
+++ Nomeclatura/main.c
@@ -3,6 +3,21 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    //punteros
+    int *punteroNumero;
+    int numero;
+    int num2;
+    num2=&numero;
+    numero=66;
+    punteroNumero=&numero;
+    printf("\n a- %d",num2);
+    //printf("\n %d",numero);
+    num2=*punteroNumero;
+    printf("\n b- d%",num2);
+    printf("\n %p",&numero);
+    //punteroNumero=55;
+    printf("\n %p",*punteroNumero);
+    printf("\n %p",&punteroNumero);
+    printf("\n %d",*punteroNumero);
     return 0;
 }
